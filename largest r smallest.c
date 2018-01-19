ed2d6510d46bc73d118cf5539c953792567ff079
VigneshTNS <31506085+VigneshTNS@users.noreply.github.com>
2018-01-19 16:43:35 +0530

    Update largest r smallest.c

--- largest r smallest.c
+++ largest r smallest.c
@@ -1,9 +1,9 @@
 int main()
 {
 int a,b,c;
-printf("please enter a number:",a);
-printf("plese enter a number:",b);
-printf("please enter anumber:",c);
+printf("please enter a number:a",a);
+printf("plese enter a number:b",b);
+printf("please enter anumber:c",c);
 scanf("%d%d%d",&a,&b,&c);
 if((a>b)&&(a>c))
 {
