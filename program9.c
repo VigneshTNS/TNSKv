155bdb3a39cf9b65b5fb2b3e264576b0afa40dc4
VigneshTNS <31506085+VigneshTNS@users.noreply.github.com>
2018-01-28 20:55:38 +0530

    Create program9.c

---
+++ program9.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() 
+{
+int a[1000],n,k,j,s=0;
+printf("please enter the two number:");
+scanf("%d%d",&n,&k);
+printf("PLease enter the array numbers");
+for(j=0;j<n;j++)
+{
+	
+	scanf("%d",&a[j]);
+}
+for(j=0;j<k;j++);
+
+	s = s+ a[j];
+
+printf(" sum of kth value %d",s);
+	
+}
