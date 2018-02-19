3df0bde4f3c3918d2c30226ebc1f4b183797619d
VigneshTNS <31506085+VigneshTNS@users.noreply.github.com>
2018-02-19 16:39:11 +0530

    Update sorting of numbers.c

--- sorting of numbers.c
+++ sorting of numbers.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 int main()
 {
-int i,j,a[10],temp,b;
+int i,j,b,a[b];
+  float temp;
 printf("\n Please Enter the range");
 scanf("%d",&b);
 printf("\nEnter the array values");
-for(i=0;i<=b;i++)
+for(i=1;i<=b;i++)
 {
 scanf("%d",&a[i]);
 }
-for(i=0;i<=b;i++)
+for(i=1;i<=b;i++)
 {
 for(j=i+1;j<=b;j++)
 {
@@ -19,5 +20,9 @@ temp=a[i];
 a[i]=a[j];
 a[j]=temp;
 }}}
+  for(i=0;i<=b;i++)
+  {
 printf("\n the sorted numbers is %d",temp);
+}
+  return 0;
 }
