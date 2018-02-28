1ab9d17243b34a31af3e5ebf33b5a74594d35a8a
VigneshTNS <31506085+VigneshTNS@users.noreply.github.com>
2018-02-28 15:40:59 +0530

    Update fibonacciseries.c

--- fibonacciseries.c
+++ fibonacciseries.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
 int main()
 {
-int a,b,c,n;
+int i,a,b,c,n;
 a=0;b=1;
 printf(" enter the number of elements:");
 scanf("%d",&n);
-printf("\n%d \n %d",a,b);
+ printf("\n fibonacci series is:");
 for(i=1;i<=n;i++)
 {
+  printf("\n%d",a);
 c=a+b;
 a=b;
 b=c;
 }
-printf("\n fibonacci series is :%d",a);
+return 0;
 }
