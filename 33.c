8a45811f5937642ebb6d7672871cff696dea418a
VigneshTNS <31506085+VigneshTNS@users.noreply.github.com>
2018-03-02 16:16:46 +0530

    Create 33.c

---
+++ 33.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int i,count=0;
+char a[50];
+printf("Enter a sentence :");
+scanf("%[^\n]s",a);
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]==' ')
+{
+count =count+1;
+}
+}
+printf("\n number of spaces =%d",count);
+return 0;
+}
+
