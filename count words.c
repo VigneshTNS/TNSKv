5351a4128a290ccd6cd48e04312a5b6dca54c001
VigneshTNS <31506085+VigneshTNS@users.noreply.github.com>
2018-03-02 16:32:50 +0530

    Update count words.c

--- count words.c
+++ count words.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
-#include<string.h>
 int main()
 {
-int i,count=0,d;
-char a[100];
-printf("please Enter different words:");
-scanf("[\^n]%s",a[i]);
-for(i=0;i<=a[i]='\0';i++)
-  if(a[i]==' ')
-  {
-    count =count+1;
-  }
-  d=count+1;
-  printf("\n number of words =%d",d);
-  return 0;
+int i,count=0;
+char a[50];
+printf("Enter a sentence :");
+scanf("%[^\n]s",a);
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]==' ')
+{
+count =count+1;
+}
+}
+printf("\n number of spaces =%d",count+1);
+return 0;
 }
