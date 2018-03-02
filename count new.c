b313e4ad02cdcd9cd6156cf0dcde8a9309212439
VigneshTNS <31506085+VigneshTNS@users.noreply.github.com>
2018-03-02 15:58:56 +0530

    Update and rename count new.c to count words.c

--- count new.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-int i,count=0,b,c;
-char a[100];
-printf("Enter different words:");
-scanf("%s",b);
-c=strlen(b);
---
+++ count words.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int i,count=0,c,d;
+char a[100];
+printf("Enter different words:");
+scanf("%s",&a[i]);
+c=strlen(a);
+for(i=0;i<=c;i++)
+  if(a[i]=='32')
+  {
+    count =count+1;
+  }
+  d=count+1;
+  printf("\n number of words =%d",d);
+  return 0;
+}
