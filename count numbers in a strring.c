abd79bb372f4866f902374f3e19ec5e5e401b4c5
VigneshTNS <31506085+VigneshTNS@users.noreply.github.com>
2018-03-04 16:18:32 +0530

    Update count numbers in a strring.c

--- count numbers in a strring.c
+++ count numbers in a strring.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 int main()
 {
-int i;
-char a[50],count=0;
+int i,count=0;
+char a[50];
 printf("Enter a string:");
 scanf("%[^\n]s",a);
 for(i=0;a[i]!='\0';i++)
 {
-if(i>47 && i<58)
+if(a[i]>47 && a[i]<58)
 {
 count=count+1;
 }
 }
-printf("\n numbers in a string :%d",count);
+printf("\n numbers in your given string :%d",count);
 return 0;
 }
