6d7b1836721212ce49557be955c5ecd8a32dec02
vigneshTNS <31506085+VigneshTNS@users.noreply.github.com>
2018-03-04 16:54:05 +0530

    Create 41.c

---
+++ 41.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int i,k;
+char s[50];
+printf(" Enter a string:");
+scanf("%s",&s[i]);
+printf("\n Enter number of times:");
+scanf("%d",&k);
+for(i=0;i<=k;i++)
+{
+printf("\n %s",s);
+}
+return 0;
+}
