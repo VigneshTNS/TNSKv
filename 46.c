33ded7fee984cc534fce7a0c1a46e958b3fdc6de
vigneshTNS <31506085+VigneshTNS@users.noreply.github.com>
2018-03-12 16:27:20 +0530

    Create 46.c

---
+++ 46.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b;
+clrscr();
+printf("Enter a number");
+scanf("%d",&a);
+b=a+1;
+printf("the next number is %d",b);
+getch();
+return 0;
+}
