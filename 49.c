09583b7d1edee0a8c445d5c449029e0ddf64c4d4
vigneshTNS <31506085+VigneshTNS@users.noreply.github.com>
2018-03-15 22:32:23 +0530

    Create 49.c

---
+++ 49.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b;
+clrscr();
+printf("Enter any two numbers");
+scanf("%d%d",&a,&b);
+printf("\nAddition of %d and %d is %d",a,b,a+b);
+getch();
+return 0;
+}
